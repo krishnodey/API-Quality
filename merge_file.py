6d9d3b1c686dd7d4ed789989a6071d12a0193ea0
Krishno Dey <dey.krishno25@gmail.com>
2024-08-29 10:12:55 -0300

    data added

--- merge_file.py
+++ merge_file.py
@@ -5,6 +5,7 @@ import json
 output_file = 'All-Data\\output_data.jsonl'
 
 base_path = ["REST-APIs", "GraphQL-APIs"]
+i = 1
 with open(output_file, 'w', encoding='utf-8') as outfile:
     for api in base_path:
         api_path = f"{api.strip()}/APIList.txt"
@@ -19,7 +20,9 @@ with open(output_file, 'w', encoding='utf-8') as outfile:
                 for line in infile:
                     # Load the JSON object
                     line_dict = json.loads(line)
+                    line_dict['id'] = i
                     json_string=json.dumps(line_dict,ensure_ascii=False)
                     outfile.write(json_string + '\n')
+                    i = i + 1
             
     print(f'Merged all files into {output_file}')
